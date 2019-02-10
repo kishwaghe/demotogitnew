80dcfe011cd712720261f7c9425a2f5c33c443da
kishwaghe <44090084+kishwaghe@users.noreply.github.com>
2019-02-10 15:02:59 +0530

    Update Test.java

--- Spring-sessionusingjdbc/src/main/java/com/main/Test.java
+++ Spring-sessionusingjdbc/src/main/java/com/main/Test.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test {
+	adding msgggggg
 public static void main(String[] args) {
 	SpringApplication.run(Test.class, args);
 }
